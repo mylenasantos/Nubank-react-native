0b04604caa411cd92413034c53612fe088b74acb
Mylena Santos <mylenasantos@Mylenas-MacBook-Air.local>
2019-11-15 16:13:18 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../assets/Nubank_Logo.png';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, Top, Logo, Title} from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Top>
+        <Logo source={logo} />
+        <Title>Mylena</Title>
+      </Top>
+      <Icon name="keyboard-arrow-down" size={20} color="#fff" />
+    </Container>
+  );
+}
